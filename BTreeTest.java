247bd5eb99fb9cf643a5ff9b968bd82eb4dc0b47
Casey Ryan Newsom <cnewsom@node60.boisestate.edu>
2014-12-05 09:37:48 -0700

    added search after random test

--- BTreeTest.java
+++ BTreeTest.java
@@ -2,9 +2,9 @@ import java.util.Random;
 public class BTreeTest{
     private static Random numGen;
     public static void main(String[] args){
-        BTree mbt = new BTree(2,"test");
+        BTree mbt = new BTree(128,"test1");
         numGen = new Random();
-        for (int i = 1; i < 11; i++){
+        for (int i = 1; i < 1000; i++){
             mbt.insert(i);
             mbt.inOrderPrint(mbt.getRoot());
             System.out.println();
@@ -12,20 +12,28 @@ public class BTreeTest{
             mbt.inOrderPrint(mbt.getRoot());
             System.out.println();
         }
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i < 1000; i++) {
         TreeObject a = mbt.search(mbt.getRoot(),i);
         if (a == null || a.compareTo(new TreeObject(i)) != 0)
             System.err.println("search failed!");
         }
-        mbt = new BTree(2,"test");
-        for (int i = 1; i < 11; i++){
+        long[] keys = new long[100];
+        mbt = new BTree(2,"test2");
+        for (int i = 0; i < 100; i++){
             int num = numGen.nextInt(15);
             mbt.insert(num);
+            keys[i] = num;
             mbt.inOrderPrint(mbt.getRoot());
             System.out.println();
             mbt.insert(num);
             mbt.inOrderPrint(mbt.getRoot());
             System.out.println();
+        }
+        for (int i = 0; i < 100; i++) {
+        
+        TreeObject a = mbt.search(mbt.getRoot(),keys[i]);
+        if (a == null || a.compareTo(new TreeObject(keys[i])) != 0)
+            System.err.println("search failed!");
         }
     }
 }
