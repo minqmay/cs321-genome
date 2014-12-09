minqmay/cs321-genome

commit 350317ce2ac4b5e2e1e0da94199f09f4a6223996

set charPosition pointer correctly

GeneBankCreateBTree.java
@@ -110,14 +110,16 @@ public class GeneBankCreateBTree {
                 if (line.startsWith("//")) { // marks the end of the sequence
                     inSequence = false;
                     if (sequencePosition > 0) {
-	                    // insert our partial sequence
+	                    //insert our partial sequence
 	                    tree.insert(sequence);
 	                    sequencePosition = 0;
 	                    sequence = 0;
                     }
                 } else {
+                    System.out.println(line);
                 	while (charPosition < line.length()) {
                         char c = line.charAt(charPosition++);
+                        System.out.println("c: " + c);
                         switch (c) {
 	                        case 'a':
 	                            sequence = ((sequence<<2) | CODE_A);
@@ -146,6 +148,7 @@ public class GeneBankCreateBTree {
 	                        tree.insert(sequence);
 	                        sequencePosition = 0;
 	                        sequence = 0;
+                            charPosition = charPosition - sequenceLength + 1;
                         }
                 	}
                 }

commit d8a10a77ad1ecac8a8c1226fed50a0daefa706e0

Add all subsequences to the tree.
This fixes the bug introduced in 350317c.

GeneBankCreateBTree.java
@@ -112,39 +112,34 @@ public class GeneBankCreateBTree {
                     sequence = 0;
                     sequencePosition = 0;
                 } else {
-                    System.out.println(line);
                 	while (charPosition < line.length()) {
                         char c = line.charAt(charPosition++);
                         switch (c) {
 	                        case 'a':
 	                            sequence = ((sequence<<2) | CODE_A);
-	                            sequencePosition++;
+	                            if (sequencePosition < sequenceLength) sequencePosition++;
 	                            break;
 	                        case 't':
 	                            sequence = ((sequence<<2) | CODE_T);
-	                            sequencePosition++;
+	                            if (sequencePosition < sequenceLength) sequencePosition++;
 	                            break;
 	                        case 'c':
 	                            sequence = ((sequence<<2) | CODE_C);
-	                            sequencePosition++;
+	                            if (sequencePosition < sequenceLength) sequencePosition++;
 	                            break;
 	                        case 'g':
 	                            sequence = ((sequence<<2) | CODE_G);
-	                            sequencePosition++;
+	                            if (sequencePosition < sequenceLength) sequencePosition++;
 	                            break;
-	                        case 'n':
+	                        case 'n': // end of subsequence
 	                            sequencePosition = 0;
                                 sequence = 0;
 	                            break;
 	                        default: // space or number, not part of sequence
 	                            break;
                         }
-                        if (sequencePosition == sequenceLength) {
-	                        // we completed a sequence
-	                        tree.insert(sequence);
-	                        sequencePosition = 0;
-	                        sequence = 0;
-                            charPosition = charPosition - sequenceLength + 1;
+                        if (sequencePosition >= sequenceLength) {
+	                        tree.insert(sequence & (~(0xffffffffffffffffL<<(sequenceLength<<1))));
                         }
                 	}
                 }
@@ -165,7 +160,7 @@ public class GeneBankCreateBTree {
 			writer.close();
 		}
 		
-	    tree.inOrderPrint(tree.getRoot());	
+	    //tree.inOrderPrint(tree.getRoot());	
 		in.close();
 	}
 	